a29c6443b359e2e4e6976cd4544305f240540352
Aunnur Sakkhor <34005081+aunnursakkhor@users.noreply.github.com>
2020-05-30 03:18:49 +0600

    Adding images

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,12 +7,14 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
 
-    <h1>Learn to design and code React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-
-    <Link to="/page-2/">Watch the video</Link>
+        <Link to="/page-2/">Watch the video</Link>
+      </div>
+    </div>
   </Layout>
 )
 
