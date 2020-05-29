a9dd38d3ea3a7217b79265e4a72a7214c0698429
Aunnur Sakkhor <34005081+aunnursakkhor@users.noreply.github.com>
2020-05-30 01:18:46 +0600

    body style

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,10 +12,6 @@ const IndexPage = () => (
     <h1>Learn to design and code React apps</h1>
     <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-
     <Link to="/page-2/">Watch the video</Link>
   </Layout>
 )
